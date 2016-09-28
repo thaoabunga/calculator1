062650807ca303395aa44fa841385a8d8a80168a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 12:25:44 -0700

    implemented functions in arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,31 @@
+def add(num1, num2):
+	"""Returns the sum of the inputs"""
+    return num1 + num2
+
+def subtract(num1, num2):
+	"""Returns the result of subtracting the second number from the first"""
+    return num1 - num2
+
+def multiply(num1, num2):
+	"""Returns the product of the inputs"""
+    return num1 * num2
+
+def divide(num1, num2):
+	"""Divides the first input by the second, returning a floating point number"""
+    return num1 / num2
+
+def square(num1):
+	"""Returns the square of the input"""
+    return num1 * num1
+
+def cube(num1):
+	"""Returns the cube of the input"""
+    return num1 ** 3
+
+def power(num1, num2):
+    """Returns the result of raising the first input to the power of the second input"""
+    return num1 ** num2
+
+def mod(num1, num2):
+	"""Returns the remainder when the first input is divided by the second input"""
+    return num1 % num2
